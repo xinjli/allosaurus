d3e00d412bcdd5294bbf415c53692932aefabe3a
lixinjian <lixinjian1217@gmail.com>
2020-09-18 14:39:29 -0400

    update README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
    name='allosaurus',
-   version='0.1.4',
+   version='0.1.5',
    description='a multilingual phone recognizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
