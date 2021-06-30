7c1dac1f8e9ef2b658d709b3360e9adffd67c384
lixinjian <lixinjian1217@gmail.com>
2021-06-29 20:18:49 -0400

    update version and gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
    name='allosaurus',
-   version='1.0.0',
+   version='1.0.1',
    description='a multilingual phone recognizer',
    author='Xinjian Li',
    author_email='xinjianl@cs.cmu.edu',
